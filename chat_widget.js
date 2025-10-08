5ddc7c5f4fc4c789bc72ace4e3a5637cf48d6fff
Naveen Aggarwal <naveen.aggarwal@gmail.com>
2025-10-08 13:09:53 -0500

    fix emojis

--- chat_widget.js
+++ chat_widget.js
@@ -61,6 +61,10 @@
     const str = String(text || '');
     const div = document.createElement('div');
     div.textContent = str;
+    // For action button text, preserve emojis by returning the original string if it contains emojis
+    if (/[\u{1F600}-\u{1F64F}]|[\u{1F300}-\u{1F5FF}]|[\u{1F680}-\u{1F6FF}]|[\u{1F1E0}-\u{1F1FF}]|[\u{2600}-\u{26FF}]|[\u{2700}-\u{27BF}]/u.test(str)) {
+      return str;
+    }
     return div.innerHTML;
   }
 
